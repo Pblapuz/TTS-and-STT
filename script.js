6fce4e3a7a8fdeb3da37ee26640c51ff5b315905
Pblapuz <pblapuzmain+github@gmail.com>
2024-08-17 15:39:30 +0800

    STT implemented

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const synth = window.speechSynthesis;
 const voiceSelect = document.getElementById("select");
 const listenButton = document.getElementById("listen");
 const inputText = document.getElementById("input-text");
-const downloadButton = document.getElementById("download");
+const outputText = document.getElementById("output-text");
+const switchButton0 = document.getElementById("switch_0");
+const switchButton1 = document.getElementById("switch_1");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+
+window.SpeechRecognition = window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition;
+recognition.interimResults = true;
+recognition.continuous = true;
 
 function voiceList() {
     voices = synth.getVoices();
@@ -21,6 +30,33 @@ function listen(){
     synth.speak(speech);
 }
 
+function startTranscript(){
+    recognition.addEventListener("result", e => {
+        const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript);
+    
+        outputText.innerHTML = transcript;
+    });
+        recognition.start();
+    
+}
+function stopTranscript(){
+    recognition.abort();
+}
+function switchToTTS(){
+    document.getElementById("panel_0").style.display = "none";
+    document.getElementById("panel_1").style.display = "flex";
+}
+function switchToSTT(){
+    document.getElementById("panel_0").style.display = "flex";
+    document.getElementById("panel_1").style.display = "none";
+}
+
 synth.addEventListener("voiceschanged", voiceList);
 voiceSelect.addEventListener("change", changeVoice);
 listenButton.addEventListener("click", listen);
+switchButton0.addEventListener("click", switchToTTS);
+switchButton1.addEventListener("click", switchToSTT);
+startButton.addEventListener("click",startTranscript);
+stopButton.addEventListener("click",stopTranscript);
